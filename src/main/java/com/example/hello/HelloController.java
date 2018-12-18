4fc94a775d015c5414dbc533bfcbc28a75713902
1427231253 <1427231253@qq.com>
2018-12-18 15:31:07 +0800

    提交hello

---
+++ src/main/java/com/example/hello/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zr
+ * @ClassName: HelloController
+ * @Description: TODO
+ * @date 2018/12/18 14:39
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String hello(){
+        return "Hello,Jenkins!";
+    }
+}
