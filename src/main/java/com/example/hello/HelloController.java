bf280b8de74c53f995b3ffbd2c3625f54ca0b8f4
zr9883 <1427231253@qq.com>
2018-12-19 10:22:06 +0800

    Update HelloController.java

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @RequestMapping("hello")
     public String hello(){
-        return "Hello,Jenkins!1234567";
+        return "Hello,Jenkins!qqqqqqqqqqqqqq";
     }
 }
